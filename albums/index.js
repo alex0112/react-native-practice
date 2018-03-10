c35c913d53799d93a86eb5527e62ae064fdd3042
alex0112 <alarsen0112@gmail.com>
2018-03-09 23:12:29 -0700

    re-wrote index.js

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,13 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// Import Libraries
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
 
+// Create Component
+const App = () => {
+    return(
+	<Text>Hello, World.</Text>
+    );
+};
+
+// Render Component
 AppRegistry.registerComponent('albums', () => App);
